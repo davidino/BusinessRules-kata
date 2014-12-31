b227ef3bbe06bc26b21464c186d435e51292c6aa
David Funaro <ing.davidino@gmail.com>
2014-12-31 16:41:05 +0100

    behat first configuration

---
+++ features/bootstrap/Acme/DemoBundle/Features/Context/FeatureContext.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Acme\DemoBundle\Features\Context;
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\Symfony2Extension\Context\KernelDictionary;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context, SnippetAcceptingContext
+{
+    use KernelDictionary;
+    
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+}
